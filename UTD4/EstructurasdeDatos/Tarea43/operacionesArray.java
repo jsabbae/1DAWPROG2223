4f38e4524af0b525f2d500a81947bfd74d220e2e
juanma785 <115530852+juanma785@users.noreply.github.com>
2023-01-25 19:58:07 +0100

    Add files via upload

---
+++ UTD4/EstructurasdeDatos/Tarea43/operacionesArray.java
@@ -0,0 +1,19 @@
+package UTD4.Tarea42.Tarea43;
+
+public class operacionesArray {
+    public static void main(String[] args) {
+        // Crea un array con 10 números aleatorios
+        int[] numeros = new int[10];
+        NumeroAleat numaleat = new NuemeroAleat();
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = numaleat.nextInt();
+        }
+        for (int i = numeros.length - 1; i >= 0; i--) {
+            System.out.println(numeros[i]);
+
+            int[] a = {1,2,3,4,5,6,7,8,9,10};
+
+
+        }
+    }
+}
