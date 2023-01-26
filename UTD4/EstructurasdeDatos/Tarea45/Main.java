0849215d572b9aaa16a67185e41ed8f6c2c1b187
juanma785 <115530852+juanma785@users.noreply.github.com>
2023-01-26 22:06:03 +0100

    Add files via upload

---
+++ UTD4/EstructurasdeDatos/Tarea45/Main.java
@@ -0,0 +1,13 @@
+package UTD4.Tarea45;
+
+public class Main {
+    public static void main(String[] args) {
+        Main objeto = new Main();
+        int[] arr = {7, 1, 1, 2, 9;
+        int[] out = objeto.insertarElemento(arr, 8);
+        for (int i : out{
+            System.out.print(i + " ");
+        }
+    }
+}
+}
