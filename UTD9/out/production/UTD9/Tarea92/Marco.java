03fe0c54d22b541628f8f26966d03af40374200f
juanma785 <115530852+juanma785@users.noreply.github.com>
2023-05-30 19:21:37 +0200

    Update Marco.java

--- UTD9/out/production/UTD9/Tarea92/Marco.java
+++ UTD9/out/production/UTD9/Tarea92/Marco.java
@@ -6,6 +6,7 @@ public class Marco extends Frame {
     public Marco(){
         setTitle("Flow Layout");
         setBounds(600, 350, 600, 300);
+        setLayout(new FlowLayout())
         Panel lamina = new Panel();
         add(lamina);
     }
